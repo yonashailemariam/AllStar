921df727a6a1bfd46324f67471d9ae9f249331b6
Steve Zingman N4IRS <szingman@msgstor.com>
2015-06-21 07:39:48 -0400

    Add RPi2 Wheezy support

---
+++ platforms/rpi/rpi2_wheezy/rpi2_wheezy_allstar_asterisk_install.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+#########################################################
+#                                                       #
+# script was built for rpi1 AllStar Asterisk install.   #
+#                                                       #
+#########################################################
+
+echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
+/srv/scripts/required_libs.sh
+/srv/scripts/build_tools.sh
+/srv/scripts/get_src.sh
+
+/srv/scripts/build_dahdi.sh
+/srv/scripts/build_asterisk.sh
+
+
