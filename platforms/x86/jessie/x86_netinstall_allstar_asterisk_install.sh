4f2fdcfc3b601f3c0f617c5aaf4a6ed221d69c92
Steve Zingman N4IRS <szingman@msgstor.com>
2015-09-15 08:49:01 -0400

    added netstat logging

--- platforms/x86/jessie/x86_netinstall_allstar_asterisk_install.sh
+++ platforms/x86/jessie/x86_netinstall_allstar_asterisk_install.sh
@@ -7,7 +7,28 @@
 #
 # Introducing a brief sleep makes things work right all the
 # time. This needs to be checked again and see if we can remove it.
-sleep 30
+
+# Disable /etc/network/interfaces
+cd /etc/network
+patch < /srv/patches/patch-interfaces
+echo "Disable /etc/network/interfaces" >>/var/log/automated_install
+
+# Setup systemd networking
+/srv/scripts/mk_eth0.network.sh
+echo "make systemd network" >>/var/log/automated_install
+
+# Enable and start systemd networking
+systemctl enable systemd-networkd.service
+systemctl start systemd-networkd.service
+
+sleep 10
+
+# Log UDP and TCP listeners during install process
+echo > /var/log/netstat.txt
+echo "At the top of the script" >> /var/log/netstat.txt
+echo  >> /var/log/netstat.txt
+netstat -unap >> /var/log/netstat.txt
+netstat -tnap >> /var/log/netstat.txt
 
 # DL x86 tar ball
 echo "start DL of AllStar Asterisk install" >>/var/log/automated_install
@@ -16,8 +37,6 @@ wget https://github.com/N4IRS/AllStar/raw/master/x86.tar.gz
 
 # untar x86 script
 tar zxvf x86.tar.gz
-
-# The 2 patches below may be moved to image_prep
 
 ########################################################
 
@@ -51,12 +70,15 @@ cd /etc
 patch </srv/patches/patch-x86-stock-netinstall-rc.local
 echo "put rc.local back to default" >>/var/log/automated_install
 
+# Get AllSter, DAHDI and kernel headers
 /srv/scripts/get_src.sh
 echo "Get Source" >>/var/log/automated_install
 
+# build DAHDI
 /srv/scripts/build_dahdi.sh
 echo "build DAHDI" >>/var/log/automated_install
 
+# Build Asterisk
 /srv/scripts/build_asterisk.sh
 echo "build Asterisk" >>/var/log/automated_install
 
@@ -72,16 +94,17 @@ cd /etc
 patch < /srv/patches/patch-rc.local
 
 # Disable /etc/network/interfaces
-cd /etc/network
-patch < /srv/patches/patch-interfaces
-echo "Disable /etc/network/interfaces" >>/var/log/automated_install
+# cd /etc/network
+# patch < /srv/patches/patch-interfaces
+# echo "Disable /etc/network/interfaces" >>/var/log/automated_install
 
-# Enable systemd networking
-/srv/scripts/mk_eth0.network.sh
-echo "make systemd network" >>/var/log/automated_install
+# Setup systemd networking
+# /srv/scripts/mk_eth0.network.sh
+# echo "make systemd network" >>/var/log/automated_install
 
-systemctl enable systemd-networkd.service
-systemctl start systemd-networkd.service
+# Enable and start systemd networking
+# systemctl enable systemd-networkd.service
+# systemctl start systemd-networkd.service
 
 # Reboot into the system
 echo "AllStar Asterisk install Complete, rebooting" >>/var/log/automated_install
