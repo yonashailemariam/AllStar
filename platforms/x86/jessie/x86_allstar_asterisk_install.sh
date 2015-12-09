576101857c3dcc534ada28991ad49b8665fa5da8
Steve Zingman N4IRS <szingman@msgstor.com>
2015-12-08 19:37:14 -0500

    Updated

--- platforms/x86/jessie/x86_allstar_asterisk_install.sh
+++ platforms/x86/jessie/x86_allstar_asterisk_install.sh
@@ -1,18 +1,47 @@
-#!/bin/sh
+#!/bin/sh -e
 
-sleep 10
+# This script will run the first time the system boots. Even
+# though we've told it to run after networking is enabled,
+# I've observed inconsistent behavior if we start hitting the
+# net immediately.
+#
+# Introducing a brief sleep makes things work right all the
+# time. This needs to be checked again and see if we can remove it.
+
+# No need for NFS or rpcbind
+# can this be moved to run BEFORE networking is started?
+apt-get remove nfs-common -y
+apt-get purge rpcbind -y
+apt-get autoremove -y
+
+cd /srv
+wget https://github.com/N4IRS/AllStar/raw/master/x86.tar.gz
+
+# untar x86 script
+tar zxvf x86.tar.gz
+
+# setup ntpdate
+# add hourly clock adjustment
+apt-get install ntpdate -y
+ln -s /etc/network/if-up.d/ntpdate /etc/cron.hourly/ntpdate
+
+# install required
+/srv/scripts/required_libs.sh
+
+# install build_tools
+/srv/scripts/build_tools.sh
 
 # get AllSter, DAHDI and kernel headers
 /srv/scripts/get_src.sh
-echo "Get Source" >>/var/log/install.log
 
 # build DAHDI
 /srv/scripts/build_dahdi.sh
-echo "build DAHDI" >>/var/log/install.log
+
+# build libpri
+/srv/scripts/build_libpri.sh
 
 # Build Asterisk
 /srv/scripts/build_asterisk.sh
-echo "build Asterisk" >>/var/log/install.log
 
 # make /dev/dsp available
 # not needed for a hub
@@ -23,10 +52,16 @@ echo snd_pcm_oss >>/etc/modules
 cd /etc
 patch < /srv/patches/patch-rc.local
 
+# Add asterisk to logrotate
+/srv/scripts/mk_logrotate_asterisk.sh
+
 # Reboot into the system
-echo "AllStar Asterisk install Complete, rebooting" >>/var/log/install.log
 
-sleep 5
+# setup for Phase 3
+cp /srv/post_install/* /usr/local/sbin
 
-/sbin/reboot
+touch /etc/asterisk/firsttime
 
+echo "test -e /etc/asterisk/firsttime && /usr/local/sbin/firsttime" >>/root/.bashrc
+
+echo "Please reboot now"
