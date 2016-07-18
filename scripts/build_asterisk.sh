8c2dd4c1f67a835237fa8ce05736f6486bc2592a
Steve Zingman <szingman@msgstor.com>
2016-07-17 21:21:02 -0400

    Cleanup

--- scripts/build_asterisk.sh
+++ scripts/build_asterisk.sh
@@ -8,17 +8,6 @@
 
 cd /usr/src/astsrc-1.4.23-pre/asterisk/
 
-# patch for ulaw Core and Extras Sound Packages
-patch < /srv/patches/patch-asterisk-menuselect.makeopts
-
-# patch for SSL used in res_crypto
-patch < /srv/patches/patch-configure
-patch < /srv/patches/patch-configure.ac
-
-# patch for LSB used in Debian init scripts
-patch -p1 < /srv/patches/patch-rc-debian
-patch < /srv/patches/patch-asterisk-makefile
-
 ./configure
 
 # Build and install Asterisk
